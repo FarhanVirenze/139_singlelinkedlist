68936af8bfc87fbab4664b299008eea6f26055e6
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-18 14:52:02 +0700

    tambahan fungsi if

--- 139_singlelinkedlist/139_singlelinkedlist.cpp
+++ 139_singlelinkedlist/139_singlelinkedlist.cpp
@@ -57,6 +57,12 @@ bool serachNode(int nim, Node* current, Node* previous) {
 	current = START;
 	while (current != NULL&& > current->noMhs)
 	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
 
 	}
 }
